adbaa0bdad1543316a6bb83def00530541dd776d
lmxdawn <lmxdawn@qq.com>
2021-12-17 17:35:08 +0800

    增加撮合后增加用户余额明细表

--- user/src/main/java/com/lmxdawn/user/dubbo/service/MemberCoinDubboServiceImpl.java
+++ user/src/main/java/com/lmxdawn/user/dubbo/service/MemberCoinDubboServiceImpl.java
@@ -3,9 +3,11 @@ package com.lmxdawn.user.dubbo.service;
 import com.lmxdawn.dubboapi.req.user.MemberCoinMatchDubboReq;
 import com.lmxdawn.dubboapi.res.user.MemberCoinSimpleDubboRes;
 import com.lmxdawn.dubboapi.service.user.MemberCoinDubboService;
+import com.lmxdawn.user.dao.MemberBillDao;
 import com.lmxdawn.user.dao.MemberCoinDao;
 import com.lmxdawn.user.dao.MemberDao;
 import com.lmxdawn.user.entity.Member;
+import com.lmxdawn.user.entity.MemberBill;
 import com.lmxdawn.user.entity.MemberCoin;
 import com.lmxdawn.user.service.MemberCoinService;
 import org.apache.dubbo.config.annotation.DubboService;
@@ -27,6 +29,9 @@ public class MemberCoinDubboServiceImpl implements MemberCoinDubboService {
 
     @Autowired
     private MemberCoinService memberCoinService;
+
+    @Autowired
+    private MemberBillDao memberBillDao;
 
     @Override
     public boolean createAll(Long coinId) {
@@ -83,10 +88,16 @@ public class MemberCoinDubboServiceImpl implements MemberCoinDubboService {
         Long coinId = req.getCoinId(); // 计价币种
         Long buyMemberId = req.getBuyMemberId();
         Double buyMoney = req.getBuyMoney();
+        Double buyMoneyFee = req.getBuyMoneyFee();
         Double buyUnfrozenMoney = req.getBuyUnfrozenMoney();
         Long sellMemberId = req.getSellMemberId();
         Double sellMoney = req.getSellMoney();
+        Double sellMoneyFee = req.getSellMoneyFee();
         Double sellUnfrozenMoney = req.getSellUnfrozenMoney();
+
+        String billCategory = "trade";
+        // 增加明细
+        List<MemberBill> memberBillList = new ArrayList<>();
 
         // 增加买入
         MemberCoin buyIncr = new MemberCoin();
@@ -97,6 +108,19 @@ public class MemberCoinDubboServiceImpl implements MemberCoinDubboService {
         if (!b) {
             return false;
         }
+        // 增加买入的明细
+        MemberBill buyMemberBill = new MemberBill();
+        buyMemberBill.setMemberId(buyMemberId);
+        buyMemberBill.setCoinId(tradeCoinId);
+        buyMemberBill.setCategory(billCategory);
+        buyMemberBill.setType(1);
+        buyMemberBill.setMoney(buyMoney);
+        buyMemberBill.setFee(buyMoneyFee);
+        buyMemberBill.setRemark("币币交易买入");
+        buyMemberBill.setCreateDate(new Date());
+        buyMemberBill.setCreateTime(new Date());
+        buyMemberBill.setModifiedTime(new Date());
+        memberBillList.add(buyMemberBill);
 
         // 解冻买入
         MemberCoin buyUnfrozen = new MemberCoin();
@@ -107,6 +131,19 @@ public class MemberCoinDubboServiceImpl implements MemberCoinDubboService {
         if (!unfrozen) {
             return false;
         }
+        // 增加解冻买入的明细
+        MemberBill buyUnfrozenMemberBill = new MemberBill();
+        buyUnfrozenMemberBill.setMemberId(buyMemberId);
+        buyUnfrozenMemberBill.setCoinId(coinId);
+        buyUnfrozenMemberBill.setCategory(billCategory);
+        buyUnfrozenMemberBill.setType(2);
+        buyUnfrozenMemberBill.setMoney(buyUnfrozenMoney);
+        buyUnfrozenMemberBill.setFee(0.00);
+        buyUnfrozenMemberBill.setRemark("币币交易解冻买入");
+        buyUnfrozenMemberBill.setCreateDate(new Date());
+        buyUnfrozenMemberBill.setCreateTime(new Date());
+        buyUnfrozenMemberBill.setModifiedTime(new Date());
+        memberBillList.add(buyUnfrozenMemberBill);
 
         // 增加卖出
         MemberCoin sellIncr = new MemberCoin();
@@ -117,13 +154,45 @@ public class MemberCoinDubboServiceImpl implements MemberCoinDubboService {
         if (!b1) {
             return false;
         }
+        // 增加卖出的明细
+        MemberBill sellMemberBill = new MemberBill();
+        sellMemberBill.setMemberId(sellMemberId);
+        sellMemberBill.setCoinId(coinId);
+        sellMemberBill.setCategory(billCategory);
+        sellMemberBill.setType(1);
+        sellMemberBill.setMoney(sellMoney);
+        sellMemberBill.setFee(sellMoneyFee);
+        sellMemberBill.setRemark("币币交易卖出");
+        sellMemberBill.setCreateDate(new Date());
+        sellMemberBill.setCreateTime(new Date());
+        sellMemberBill.setModifiedTime(new Date());
+        memberBillList.add(sellMemberBill);
 
         // 解冻卖出
         MemberCoin sellUnfrozen = new MemberCoin();
         sellUnfrozen.setMemberId(sellMemberId);
         sellUnfrozen.setCoinId(tradeCoinId);
         sellUnfrozen.setFrozenBalance(sellUnfrozenMoney);
-        return memberCoinDao.unfrozen(sellUnfrozen);
+        boolean b2 = memberCoinDao.unfrozen(sellUnfrozen);
+        if (!b2) {
+            return false;
+        }
+
+        // 增加解冻卖出的明细
+        MemberBill sellUnfrozenMemberBill = new MemberBill();
+        sellUnfrozenMemberBill.setMemberId(sellMemberId);
+        sellUnfrozenMemberBill.setCoinId(tradeCoinId);
+        sellUnfrozenMemberBill.setCategory(billCategory);
+        sellUnfrozenMemberBill.setType(2);
+        sellUnfrozenMemberBill.setMoney(sellUnfrozenMoney);
+        sellUnfrozenMemberBill.setFee(0.00);
+        sellUnfrozenMemberBill.setRemark("币币交易解冻卖出");
+        sellUnfrozenMemberBill.setCreateDate(new Date());
+        sellUnfrozenMemberBill.setCreateTime(new Date());
+        sellUnfrozenMemberBill.setModifiedTime(new Date());
+        memberBillList.add(sellUnfrozenMemberBill);
+
+        return memberBillDao.insertBatch(memberBillList);
     }
 
 
