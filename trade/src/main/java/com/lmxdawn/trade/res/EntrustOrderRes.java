c3ee224828152caeac477b3619e97b9d6036421c
lmxdawn <lmxdawn@qq.com>
2022-05-18 07:26:51 +0800

    增加已完成交易额的返回

--- trade/src/main/java/com/lmxdawn/trade/res/EntrustOrderRes.java
+++ trade/src/main/java/com/lmxdawn/trade/res/EntrustOrderRes.java
@@ -40,6 +40,9 @@ public class EntrustOrderRes {
     @ApiModelProperty(value = "交易额")
     private Double total;
 
+    @ApiModelProperty(value = "已完成交易额")
+    private Double totalComplete;
+
     @ApiModelProperty(value = "状态（1：未完成，2：已完成）")
     private Integer status;
 
